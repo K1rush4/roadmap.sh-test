8d4471c513415060ac111f26af2f005ab455d48d
K1rush4 <gomzikovKD@mail.ru>
2024-05-23 14:21:28 +0500

    commit with new code

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 function App() {
 
     return (
-        <div>
-            first
-            <p id="result"></p>
-        </div>
+        <>
+            <div>
+                first
+                <p id="result"></p>
+            </div>
+            <div>some new code here</div>
+        </>
     );
 }
 
